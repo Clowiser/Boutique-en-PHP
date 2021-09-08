c805df664333753dfab902a5f8fe116cda611dac
Jessica <jessica.rigaud@le-campus-numerique.fr>
2021-09-08 20:23:07 +0200

    Nom du dossier renommé html->Php + modification des chemin dans code

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 session_start();
-require("html/fonctions.php"); // ou required 'fonctions.php'; inclue = on a besoin ; identique à include sauf que si erreur = ERREUR FATALE et stope le script - on inclue le page des fonctions.php dans l'ensemble du site, et chacune des fonctions est appelé dans les pages où elles sont utilisées
+require("php/fonctions.php"); // ou required 'fonctions.php'; inclue = on a besoin ; identique à include sauf que si erreur = ERREUR FATALE et stope le script - on inclue le page des fonctions.php dans l'ensemble du site, et chacune des fonctions est appelé dans les pages où elles sont utilisées
 
-include("html/header.php"); // ou include 'header.php'; include = on affiche ; l'ensemble des fichiers du header est inclu suivant le chemin du fichier fourni dans tous les pages du site via l'index (toutes le spages du site auront le header affiché)
+include("php/header.php"); // ou include 'header.php'; include = on affiche ; l'ensemble des fichiers du header est inclu suivant le chemin du fichier fourni dans tous les pages du site via l'index (toutes le spages du site auront le header affiché)
 
     if (isset($_GET['page'])){ // moyen pour vérifier que notre variable existe $_Get['page'] - isset détermine si une variable est déclarée et est différente de null
     if ($_GET['page'] == 'index.php'){ //si page = index alors affiche accueil (index=accueil) + méthode GET pour transporter le paramètre dans la chaîne d’URL
-        include ("accueil.php");
+        include ("php/accueil.php");
     }
     if ($_GET['page'] == 'catalogue.php'){ // $_GET : variable d'environnement 
-        include ("html/catalogue.php");
+        include ("php/catalogue.php");
         echo '<title>Catalogue de la Boutique des Etoiles</title>'; 
     }
     if ($_GET['page'] == 'article.php'){
-        include ("html/article.php");
+        include ("php/article.php");
         echo '<title>Article de la Boutique des Etoiles</title>'; 
     }
     if ($_GET['page'] == 'AddArticle.php'){
-        include ("html/AddArticle.php");
+        include ("php/AddArticle.php");
         echo '<title>Ajouter un nouvel article</title>'; 
     }
     if ($_GET['page'] == 'displayarticle.php'){
-        include ("html/displayarticle.php");
+        include ("php/displayarticle.php");
         echo '<title>Votre article ajouté</title>'; 
     }
     if ($_GET['page'] == 'panier.php'){
-        include ("html/panier.php");
+        include ("php/panier.php");
         echo '<title>Votre panier </title>'; 
     }
     }else{
-        include ("accueil.php");
+        include ("php/accueil.php");
     }
 
-include("html/footer.php"); // idem que le header
+include("php/footer.php"); // idem que le header
 if(isset($_GET['destroy']))session_destroy();
 ?>
 </body>
--- php/accueil.php
+++ php/accueil.php
@@ -12,7 +12,7 @@
     <div class="contain_center">
 <div class="info">
     <h2> Plongez-vous dans notre boutique des étoiles pour découvrir un monde plein de couleurs et de lumières ! </h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <p>Hello in Star Shop ! Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
 </div>
 </div>
